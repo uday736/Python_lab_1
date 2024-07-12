11710e1f56cc6bd84671a3c749fbf719ef3bddaa
uday736 <bca2022uday1649@tnraocollege.org>
2024-07-12 20:22:17 +0530

    Add files via upload

---
+++ interest.py
@@ -0,0 +1,5 @@
+p=float(input("Enter the principal amount:"))
+r=float(input("Enter the annual rate :"))
+t=float(input("Eneter the time period:"))
+interest = p*r*t/100
+print("simple interest :",interest)
